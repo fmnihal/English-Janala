e6b97eb1d1c88cc84da828a3eeaf1ac82df2d664
fmnihal <fattahmahmudnihal@gmail.com>
2025-09-07 15:38:03 +0600

    level container added

--- script/index.js
+++ script/index.js
@@ -2,13 +2,51 @@ const loadLessons=()=>{
     fetch('https://openapi.programming-hero.com/api/levels/all')
     .then((res)=>res.json())
     // .then((data)=>console.log(data));
-    .then((data)=>console.log(data.data));
+    .then((data)=>displayLesson(data.data));
 }
-const displayLesson=(){
+const displayLesson=(lessons)=>{
     // 1. get the container and empty it
     // 2. get into every lessons
     // 3. create element
     // 4. append them to container
-    
+    const levelContainer= document.getElementById('level-container');
+    levelContainer.innerHTML='';
+    for (let lesson of lessons){
+        const btnDiv= document.createElement('div');
+        btnDiv.innerHTML=`
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+        `;
+        levelContainer.append(btnDiv);
+    }
 }
 loadLessons();
+
+const loadLevelWord=(id)=>{
+    // console.log(id);
+    const url= `https://openapi.programming-hero.com/api/level/${id}`;
+    // console.log(url);
+    fetch(url)
+    .then((res)=>res.json())
+    .then(json=>displayLevelWord(json.data));
+}
+const displayLevelWord= (words)=>{
+    // console.log(words);
+    const wordContainer= document.getElementById('word-container');
+    wordContainer.innerHTML='';
+    words.forEach((word)=>{
+        // console.log(word);
+        const card= document.createElement('div');
+        card.innerHTML=`
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="text-2xl font-bold">Hesitate</h2>
+            <p>Meaning/Pronunciation</p>
+            <h2 class="font-bn text-2xl font-bold">"দ্বিধা করা / হেজিটেট"</h2>
+            <div class="flex justify-between items-center">
+                <button class="btn-info bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn-speak bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `;
+        wordContainer.append(card);
+    });
+}
